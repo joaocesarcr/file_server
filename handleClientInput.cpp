8be1cb66f1050cc2579ee2b8a28b42179fc954b1
João César <joaocesarcriscolo@gmail.com>
2023-06-28 20:23:03 -0300

    handleClient using public variables

--- handleClientInput.cpp
+++ handleClientInput.cpp
@@ -9,141 +9,132 @@
 
 using namespace std;
 
-// Function prototypes
-void handleUpload();
+class CommandHandler {
+public:
+    int socket;
+    MESSAGE message;
 
-void handleDownload();
+    void handleUpload() {
+        printf("Upload command selected.\n");
+        // Your upload code here
+    }
 
-void handleDelete(MESSAGE message);
+    void handleDownload() {
+        printf("Download command selected.\n");
+        // Your download code here
+    }
 
-void handleLs(MESSAGE message, int socket);
+    void handleDelete() {
+        char returnMessage[MAX_MESSAGE_LENGTH + 1];
+        int n = 0;
+        printf("Delete command selected.\n");
+        printf("Client name: %s\n", message.client);
 
-void handleLc();
+        char location[256] = "server_files/"; // Declare 'location' as an array of characters
+        strcat(location, message.client);
+        printf("Location: %s\n", location);
 
-void handleGsd();
+        DIR *d;
+        struct dirent *dir;
+        d = opendir(location);
 
-int handleInput(MESSAGE message);
+        if (d) {
+            while ((dir = readdir(d))) {
+                if (dir->d_name == message.splitCommand[1]) {
+                    strcat(location, "/");
+                    strcat(location, dir->d_name);
+                    remove(location);
+                    printf("Successfully deleted file: %s\n\n", message.splitCommand[1].c_str());
+                    closedir(d);
+                    return;
+                }
+            }
+            printf("Fail to delete file [%s]: not found\n\n", message.splitCommand[1].c_str());
+            closedir(d);
+        }
+        snprintf(returnMessage, sizeof(returnMessage), "%s deleted\n", message.splitCommand[1].c_str());
+        n = write(socket, returnMessage, MAX_MESSAGE_LENGTH);
+    }
 
-std::vector<std::string> splitString(const std::string &str) {
-    string s;
+    void handleLs() {
+        printf("LS command selected!\n");
+        printf("Client name: %s\n", message.client);
 
-    stringstream ss(str);
+        char location[256] = "server_files/"; // Declare 'location' as an array of characters
+        strcat(location, message.client);
+        printf("Location: %s\n", location);
 
-    vector<string> v;
-    while (getline(ss, s, ' ')) {
-        if (s != " ") {
-            v.push_back(s);
+        DIR *d;
+        struct dirent *dir;
+        d = opendir(location);
+        char directoryNames[50][256]; // Array to store directory names
+        int n, count = 0; // Count of directory names
+
+        if (d) {
+            while ((dir = readdir(d))) {
+                if (strcmp(dir->d_name, ".") && (strcmp(dir->d_name, ".."))) {
+                    strcpy(directoryNames[count], strcat(dir->d_name, "\n"));
+                    count++;
+                }
+            }
+            closedir(d);
+            strcpy(directoryNames[count], "");
+            n = write(socket, directoryNames, 12800);
         }
     }
 
-    return v;
-}
+    void handleLc() {
+        printf("LC command selected.\n");
+        // Your lc code here
+    }
 
-// Function implementations
-void handleUpload() {
-    printf("Upload command selected.\n");
-    // Your upload code here
-}
+    void handleGsd() {
+        printf("GSD command selected.\n");
+        // Your gsd code here
+    }
 
-void handleDownload() {
-    printf("Download command selected.\n");
-    // Your download code here
-}
+    int handleInput() {
+        // Remove \n
+        message.command[strcspn(message.command, "\n")] = 0;
 
-void handleDelete(MESSAGE message, int socket) {
-    char returnMessage[MAX_MESSAGE_LENGTH + 1];
-    int n = 0;
-    printf("Delete command selected.\n");
-    printf("Client name: %s\n", message.client);
+        message.splitCommand = splitString(message.command);
+        const string &mainCommand = message.splitCommand[0];
 
-    char location[256] = "server_files/"; // Declare 'location' as an array of characters
-    strcat(location, message.client);
-    printf("Location: %s\n", location);
+        if (mainCommand == "upload") {
+            handleUpload();
+        } else if (mainCommand == "download") {
+            handleDownload();
+        } else if (mainCommand == "delete") {
+            handleDelete();
+        } else if (mainCommand == "ls") {
+            handleLs();
+        } else if (mainCommand == "lc") {
+            handleLc();
+        } else if (mainCommand == "gsd") {
+            handleGsd();
+        } else if (mainCommand == "exit") {
+            printf("Exiting the program.\n");
+        } else {
+            printf("Invalid command.\n\n");
+        }
 
-    DIR *d;
-    struct dirent *dir;
-    d = opendir(location);
+        return 0;
+    }
 
-    if (d) {
-        while ((dir = readdir(d))) {
-            if (dir->d_name == message.splitCommand[1]) {
-                strcat(location, "/");
-                strcat(location, dir->d_name);
-                remove(location);
-                printf("Successfully deleted file: %s\n\n", message.splitCommand[1].c_str());
-                closedir(d);
-                return;
+private:
+    std::vector<std::string> splitString(const std::string &str) {
+        string s;
+
+        stringstream ss(str);
+
+        vector<string> v;
+        while (getline(ss, s, ' ')) {
+            if (s != " ") {
+                v.push_back(s);
             }
         }
-        printf("Fail to delete file [%s]: not found\n\n", message.splitCommand[1].c_str());
-        closedir(d);
+
+        return v;
     }
-    snprintf(returnMessage, sizeof(returnMessage), "%s deleted\n", message.splitCommand[1].c_str());
-    n = write(socket, returnMessage, MAX_MESSAGE_LENGTH);
-}
-
-void handleLs(MESSAGE message, int socket) {
-    printf("LS command selected!\n");
-    printf("Client name: %s\n", message.client);
-
-    char location[256] = "server_files/"; // Declare 'location' as an array of characters
-    strcat(location, message.client);
-    printf("Location: %s\n", location);
-
-    DIR *d;
-    struct dirent *dir;
-    d = opendir(location);
-    char directoryNames[50][256]; // Array to store directory names
-    int n,count = 0; // Count of directory names
-    
-    if (d) {
-        while ((dir = readdir(d))) {
-            if (strcmp(dir->d_name,".") && (strcmp(dir->d_name,".."))) {
-              strcpy(directoryNames[count], strcat(dir->d_name, "\n"));
-              count++;
-            }
-        }
-        closedir(d);
-        strcpy(directoryNames[count],"");
-        n = write(socket, directoryNames, 12800);
-    }
-}
-
-void handleLc() {
-    printf("LC command selected.\n");
-    // Your lc code here
-}
-
-void handleGsd() {
-    printf("GSD command selected.\n");
-    // Your gsd code here
-}
-
-int handleInput(MESSAGE message, int socket) {
-    // Remove \n
-    message.command[strcspn(message.command, "\n")] = 0;
-
-    message.splitCommand = splitString(message.command);
-    const string &mainCommand = message.splitCommand[0];
-
-    if (mainCommand == "upload") {
-        handleUpload();
-    } else if (mainCommand == "download") {
-        handleDownload();
-    } else if (mainCommand == "delete") {
-        handleDelete(message,socket);
-    } else if (mainCommand == "ls") {
-        handleLs(message,socket);
-    } else if (mainCommand == "lc") {
-        handleLc();
-    } else if (mainCommand == "gsd") {
-        handleGsd();
-    } else if (mainCommand == "exit") {
-        printf("Exiting the program.\n");
-    } else {
-        printf("Invalid command.\n\n");
-    }
-
-    return 0;
-}
+};
 
