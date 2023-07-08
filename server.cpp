c1f2898f2422afa4317b210c71ea9f503347084a
i564455 <guilherme.fernandes@sap.com>
2023-07-08 04:12:43 +0200

    check connection acceptance and improve error handling

--- server.cpp
+++ server.cpp
@@ -4,22 +4,36 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <map>
 
 #include "./handleClientInput.cpp"
 
 #define PORT 4000
+#define MAX_CONNECTIONS_PER_CLIENT 2
+#define MAX_TOTAL_CONNECTIONS 5
+
+typedef struct clientArgs {
+    map<string, int> clients;
+    int socket;
+    size_t activeConnectionsAmount;
+} clientArgs;
 
 void *client_thread(void *arg);
 
 int create_connection();
 
+bool checkClientAcceptance(clientArgs *args);
+
 int main(int argc, char *argv[]) {
+    size_t activeConnectionsAmount = 0;
+    struct sockaddr_in cli_addr{};
+    map<string, int> clients{};
     int sockfd, newsockfd;
     socklen_t clilen;
-    struct sockaddr_in cli_addr;
-    clilen = sizeof(struct sockaddr_in);
 
+    clilen = sizeof(struct sockaddr_in);
     sockfd = create_connection();
+
     // tells the socket that new connections shall be accepted
     listen(sockfd, 5);
     printf("Waiting accept\n");
@@ -27,13 +41,20 @@ int main(int argc, char *argv[]) {
     // get a new socket with a new incoming connection
 
     while (true) {
+        if (activeConnectionsAmount == MAX_TOTAL_CONNECTIONS) {
+            printf("Limits of active connections reached.\n");
+            continue;
+        }
+
         newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
         if (newsockfd == -1)
             printf("ERROR on accept\n");
         else {
             printf("Connection established successfully.\n\n");
             pthread_t th1;
-            pthread_create(&th1, nullptr, client_thread, &newsockfd);
+            clientArgs args = {clients, newsockfd, activeConnectionsAmount};
+            pthread_create(&th1, nullptr, client_thread, &args);
+            activeConnectionsAmount++;
         }
     }
 
@@ -42,11 +63,14 @@ int main(int argc, char *argv[]) {
 
 void *client_thread(void *arg) {
     MESSAGE message;
-    int newsockfd = *(int *) arg;
+    auto *args = (clientArgs *) arg;
+    int newsockfd = (*args).socket;
     int running = 1;
     ssize_t n;
+
+    if (!checkClientAcceptance(args)) return nullptr;
+
     while (running) {
-        /* read from the socket */
         do {
             n = read(newsockfd, (void *) &message, sizeof(message));
         } while (n < sizeof(message));
@@ -55,18 +79,21 @@ void *client_thread(void *arg) {
             printf("ERROR reading from socket\n");
             return (void *) -1;
         }
-        if (!strcmp(message.command, "exit")) {
+        if (!strcmp(message.content, "exit")) {
             printf("Ending Connection\n");
             running = 0;
         } else {
             CommandHandler handler = *new CommandHandler(newsockfd, message);
             handler.handleInput();
-            /* write in the socket */
         }
     }
+
     close(newsockfd);
+    (*args).activeConnectionsAmount--;
+
     printf("Connection ended\n");
-    return 0;
+
+    return nullptr;
 }
 
 int create_connection() {
@@ -93,4 +120,37 @@ int create_connection() {
     }
     return sockfd;
 
+}
+
+bool checkClientAcceptance(clientArgs *args) {
+    bool accepted = true;
+    MESSAGE message;
+    ssize_t n;
+
+    do {
+        n = read(socket, (void *) &message, sizeof(message));
+    } while (n < sizeof(message));
+
+    string clientName = message.client;
+    strcpy(message.content, "accepted\0");
+    size_t clientConnectionsAmount = (*args).clients.count(clientName);
+
+    if (clientConnectionsAmount) {
+        if (clientConnectionsAmount > MAX_CONNECTIONS_PER_CLIENT) {
+            printf("ERROR: %s exceeded connections quota\n", clientName.c_str());
+            strcpy(message.content, "denied\0");
+            accepted = false;
+        } else {
+            (*args).clients[clientName] += 1;
+        }
+    } else {
+        (*args).clients.insert(make_pair(clientName, 1));
+    }
+
+    n = write(socket, (void *) &message, sizeof(MESSAGE));
+    if (n < 0) {
+        printf("ERROR writing to socket\n");
+    }
+
+    return accepted;
 }
